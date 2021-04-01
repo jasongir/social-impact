5e29d26f6a1d4523f97adca47435cccaafec1130
Jason <josangorix@gmail.com>
2021-04-01 16:51:06 -0400

    started working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [countryList, setCountryList] = useState([]);
+	const [stateList, setStateList] = useState(null);
+	const [cityList, setCityList] = useState(null);
+	useEffect(() => {
+		fetch(
+			"http://api.airvisual.com/v2/countries?key=f523e477-7200-471e-bc77-f4e7f17f707a"
+		)
+			.then((res) => res.json())
+			.then((json) => setCountryList(json.data));
+	}, []);
+	return (
+		<div className="App">
+			<h1>Air Quality App</h1>
+			<h2>Find out about the air you breathe</h2>
+			{countryList.map((country) => {
+				return <div>{country.country}</div>;
+			})}
+		</div>
+	);
 }
+
+const Countries = (props) => {
+	return <section>{props.countries.f}</section>;
+};
 
 export default App;
